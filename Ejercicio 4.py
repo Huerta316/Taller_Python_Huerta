b03be357e6ff95eb89057f8ab0da17d4ae8b5002
Huerta316 <dilan_huerta12@outlook.com>
2024-11-25 16:45:52 -0600

    taller_Python

---
+++ Ejercicio 4.py
@@ -0,0 +1,13 @@
+lista = [10, 20 ,30 ,40 ,50] 
+tupla = ("Red", " Green", "Blue")
+conjunto = { "A", "B", "C", "D", "E"}
+
+print(lista)
+print(tupla)
+print(conjunto)
+
+print(lista [0])
+print(lista [1])
+print(lista [2])
+print(lista [3])
+print(lista [4])
